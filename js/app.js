826aed50c35a0621ac96f3262f35552e3ba0ac0e
gene5ive <jedigene@gmail.com>
2015-07-28 10:20:41 -0700

    initial commit. add file tree and initial page MVC.

---
+++ js/app.js
@@ -0,0 +1,67 @@
+(function ($) {
+
+  var Book = Backbone.Model.extend({
+    defaults: {
+      coverImage: "img/placeholder.png",
+      title: "Some title",
+      author: "John Doe",
+      releaseDate: "2012",
+      keywords: "JavaScript Programming"
+    }
+  });
+
+  var BookView = Backbone.View.extend({
+    tagName: "div",
+    className: "bookContainer",
+    template: $("#bookTemplate").html(),
+
+    render: function () {
+      var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
+
+      this.$el.html(tmpl(this.model.toJSON())); //this.el what we defined in tagName. use $el to get access to jQuery html() function
+      return this;
+    }
+  });
+
+  var book = new Book({
+    title: "Some title",
+    author: "John Doe",
+    releaseDate: "2012",
+    keywords: "JavaScript Programming"
+  });
+
+  bookView = new BookView({
+    model: book
+  });
+
+  var Library = Backbone.Collection.extend({
+    model:Book
+  });
+
+  var LibraryView = Backbone.View.extend({
+    el:$("#books"),
+
+    initialize:function(){
+      this.collection = new Library(books);
+      this.render();
+    },
+
+    render: function(){
+      var that = this;
+      _.each(this.collection.   models, function(item){
+        that.renderBook(item);
+      }, this);
+    },
+
+    renderBook:function(item) {
+      var bookView = new BookView({
+        model: item
+      });
+      this.$el.append(bookView.render().el);
+    }
+
+  });
+
+  $("#books").html(bookView.render().el);
+
+})(jQuery);
