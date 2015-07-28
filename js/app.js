ed303bcbbc990d56c172240b26569566ce22e8e8
gene5ive <jedigene@gmail.com>
2015-07-28 11:30:41 -0700

    add form to allow adding books and delete button to book listings.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function($) {
 
   var books = [{title:"JS The Good Parts", author:"John Doe", releaseDate:"2012", keywords:"JavaScript Programming"},
                {title:"CS The better parts", author:"John Doe", releaseDate:"2012", keywords:"CoffeeScript Programming"},
@@ -9,10 +9,10 @@
   var Book = Backbone.Model.extend({
     defaults: {
       coverImage: "img/placeholder.png",
-      title: "Some title",
-      author: "John Doe",
-      releaseDate: "2012",
-      keywords: "JavaScript Programming"
+      title: "title not entered.",
+      author: "author not entered.",
+      releaseDate: "relase date not entered.",
+      keywords: "none."
     }
   });
 
@@ -21,11 +21,23 @@
     className: "bookContainer",
     template: $("#bookTemplate").html(),
 
-    render: function () {
+    render: function() {
       var tmpl = _.template(this.template); //tmpl is a function that takes a JSON object and returns html
 
       this.$el.html(tmpl(this.model.toJSON())); //this.el what we defined in tagName. use $el to get access to jQuery html() function
       return this;
+    },
+
+    events: {
+      "click .delete": "deleteBook"
+    },
+
+    deleteBook: function() {
+      //Delete model
+      this.model.destroy();
+
+      //Delete view
+      this.remove();
     }
   });
 
@@ -36,16 +48,39 @@
   var LibraryView = Backbone.View.extend({
     el:$("#books"),
 
-    initialize:function(){
+    initialize:function() {
       this.collection = new Library(books);
       this.render();
+
+      this.collection.on("add", this.renderBook, this);
+      this.collection.on("remove", this.removeBook, this);
     },
 
-    render: function(){
+    render: function() {
       var that = this;
-      _.each(this.collection.   models, function(item){
+      _.each(this.collection.   models, function(item) {
         that.renderBook(item);
       }, this);
+    },
+
+    addBook: function(e) {
+      e.preventDefault();
+
+      var formData = {};
+
+      $("#addBook div").children("input").each(function(i, el) {
+        if ($(el).val() !== "") {
+          formData[el.id] = $(el).val();
+        }
+      });
+
+      books.push(formData);
+
+      this.collection.add(new Book(formData));
+    },
+
+    events: {
+      "click #add": "addBook"
     },
 
     renderBook:function(item) {
@@ -53,7 +88,23 @@
         model: item
       });
       this.$el.append(bookView.render().el);
-    }
+    },
+
+    removeBook: function(removedBook) {
+      var removedBookData = removedBook.attributes;
+
+      _.each(removedBookData, function(val, key) {
+        if(removedBookData[key] === removedBook.defaults[key]) {
+          delete removedBookData[key];
+        }
+      });
+
+      _.each(books, function(book) {
+        if(_.isEqual(book, removedBookData)) {
+          books.splice(_.indexOf(books, book), 1);
+        }
+      });
+    },
 
   });
 
